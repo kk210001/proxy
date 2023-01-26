6560c70dbcadc3706baf4b76fdb602cfa12e3f01
kk210001 <91827665+kk210001@users.noreply.github.com>
2023-01-26 20:52:11 +0900

    기존 테스트 코드는 client -> realSubject 로 execute() 시 1초를 기다려 약 3초의 시간이 걸렸다면 cacheProxyTest 는 중간 과정에 프록시인 CacheProxy 를 추가하여 처음 execute()만 1초가 걸리고 그 다음은 더 빨리 실행되어 2초도 안걸림 -> CacheProx y에 realSubject 에서 받은 값을 저장하였기 때문

--- src/test/java/hello/proxy/pureproxy/proxy/ProxyPatternTest.java
+++ src/test/java/hello/proxy/pureproxy/proxy/ProxyPatternTest.java
@@ -1,7 +1,9 @@
 package hello.proxy.pureproxy.proxy;
 
+import hello.proxy.pureproxy.proxy.code.CacheProxy;
 import hello.proxy.pureproxy.proxy.code.ProxyPatternClient;
 import hello.proxy.pureproxy.proxy.code.RealSubject;
+import hello.proxy.pureproxy.proxy.code.Subject;
 import org.junit.jupiter.api.Test;
 
 public class ProxyPatternTest {
@@ -9,9 +11,19 @@ public class ProxyPatternTest {
     @Test
     void noProxyTest(){
         RealSubject realSubject = new RealSubject();
-        ProxyPatternClient proxyPatternClient = new ProxyPatternClient(realSubject);
-        proxyPatternClient.execute();
-        proxyPatternClient.execute();
-        proxyPatternClient.execute();
+        ProxyPatternClient client = new ProxyPatternClient(realSubject);
+//        client.execute();
+//        client.execute();
+//        client.execute();
+    }
+
+    @Test
+    void cacheProxyTest(){
+        Subject realSubject = new RealSubject();
+        Subject cacheProxy = new CacheProxy(realSubject);
+        ProxyPatternClient client = new ProxyPatternClient(cacheProxy);
+        client.execute();
+        client.execute();
+        client.execute();
     }
 }
