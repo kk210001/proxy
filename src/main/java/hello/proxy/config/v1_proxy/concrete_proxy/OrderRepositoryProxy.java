d6e20987539d48a8258590d8cc3db263294c6199
kk210001 <91827665+kk210001@users.noreply.github.com>
2023-02-04 16:02:23 +0900

    클래스 기반 프록시 생성 및 적용 extends 를 기반으로 만드는 거라 상위 클래스가 생성자를 가지고 있으면 상속 후 생성자에 super()가 붙음

---
+++ src/main/java/hello/proxy/config/v1_proxy/concrete_proxy/OrderRepositoryProxy.java
@@ -0,0 +1,30 @@
+package hello.proxy.config.v1_proxy.concrete_proxy;
+
+import hello.proxy.app.v2.OrderRepositoryV2;
+import hello.proxy.trace.TraceStatus;
+import hello.proxy.trace.logtrace.LogTrace;
+
+public class OrderRepositoryProxy extends OrderRepositoryV2 {
+
+
+    private final OrderRepositoryV2 target;
+    private final LogTrace logTrace;
+
+    public OrderRepositoryProxy(OrderRepositoryV2 target, LogTrace LogTrace) {
+        this.target = target;
+        this.logTrace = LogTrace;
+    }
+
+    @Override
+    public void save(String itemId) {
+        TraceStatus status = null;
+        try {
+            status = logTrace.begin("OrderRepository.save()");
+            target.save(itemId);
+            logTrace.end(status);
+        } catch (Exception e) {
+            logTrace.exception(status, e);
+            throw e;
+        }
+    }
+}
