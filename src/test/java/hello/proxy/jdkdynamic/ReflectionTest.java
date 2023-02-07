6de8ddbed7c8c983229581acbef301b58148f6d1
kk210001 <91827665+kk210001@users.noreply.github.com>
2023-02-07 18:32:28 +0900

    Class.forName 을 통해 클래스의 메타정보 획득 Method.getMethod 로 해당 클래스의 메서드에 대한 메타정보 획득 Method.invoke 메타정보로 실제 인스턴스의 메서드 호출
    dynamicCall() 함수를 통해 공통 로직을 한번에 처리
    Method 를 통해 호출할 메서드의 정보가 동적으로 제공
    Object 를 통해 실제 실행할 인스턴스 정보 넘어옴

--- src/test/java/hello/proxy/jdkdynamic/ReflectionTest.java
+++ src/test/java/hello/proxy/jdkdynamic/ReflectionTest.java
@@ -3,6 +3,9 @@ package hello.proxy.jdkdynamic;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 
 @Slf4j
 public class ReflectionTest {
@@ -19,6 +22,39 @@ public class ReflectionTest {
         log.info("start");
         String result2 = target.callB();
         log.info("result={}",result2);
+    }
+
+    @Test
+    void reflection1() throws Exception{
+        Class classHello = Class.forName("hello.proxy.jdkdynamic.ReflectionTest$Hello");
+
+        Hello target = new Hello();
+
+        Method methodCallA = classHello.getMethod("callA");
+        Object result1 = methodCallA.invoke(target);
+        log.info("result1={}",result1);
+
+        Method methodCallB = classHello.getMethod("callB");
+        Object result2 = methodCallB.invoke(target);
+        log.info("result2={}",result2);
+    }
+
+    @Test
+    void reflection2() throws Exception{
+        Class classHello = Class.forName("hello.proxy.jdkdynamic.ReflectionTest$Hello");
+
+        Hello target = new Hello();
+        Method methodCallA = classHello.getMethod("callA");
+        dynamicCall(methodCallA, target);
+
+        Method methodCallB = classHello.getMethod("callB");
+        dynamicCall(methodCallB, target);
+    }
+
+    private void dynamicCall(Method method, Object target) throws Exception{
+        log.info("start");
+        Object result = method.invoke(target);
+        log.info("result-{}",result);
     }
 
     @Slf4j
